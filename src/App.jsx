d4d2dade80f3223ccf64839f516cb35427e47595
abduyus <yusuf.abdurrasheed@outlook.com>
2025-08-05 02:18:13 +0500

    Implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 function App() {
     return (
-        <div>WorldWide</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Homepage />} />
+                <Route path={'product'} element={<Product />} />
+                <Route path={'pricing'} element={<Pricing />} />
+                <Route path={'*'} element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+function PageNotFound() {
+    return (
+        <div>Not found :(</div>
+    );
+}
+
+export default PageNotFound;
