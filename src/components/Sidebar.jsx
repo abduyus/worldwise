608813ac4eb1a2ed117568adfc2d851bc3eea03e
abduyus <yusuf.abdurrasheed@outlook.com>
2025-08-06 12:57:15 +0500

    making nested routes and index routes for the app component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,15 @@ import styles from './Sidebar.module.css'
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <Logo />
             <AppNav/>
-            <p>List of cities</p>
+
+            <Outlet />
             <Footer/>
         </div>
     );
